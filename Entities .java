cc98f1e9b53ceb3a31edd0495f36465e6a11a8c4
akkii22 <98634672+akkii22@users.noreply.github.com>
2025-03-21 15:03:51 +0530

    Add files via upload

---
+++ Entities .java
@@ -0,0 +1,22 @@
+package com.example.ecommerce.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private Set<Product> products;
+}
